10532a790ce547731f7a83c5236025c471e97ef8
Gaareth <gareth04@outlook.de>
2024-04-07 17:59:32 +0200

    Fix mobile test

--- lib/tests/integration_mobile.rs
+++ lib/tests/integration_mobile.rs
@@ -5,8 +5,11 @@ mod tests_mobile_requests {
     use crate::common::env_auth;
 
     #[test]
+    #[cfg(feature = "mobile")]
     fn test_get_exams() {
         let stine = env_auth();
-        dbg!(&stine.get_exams_mobile().unwrap());
+        let exam_results = stine.get_exams_mobile()
+            .expect("Failed fetching exam result from the mobile endpoint");
+        assert!(!exam_results.exams.is_empty());
     }
 }
